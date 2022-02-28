8913ced32da0a836e56dc20006eb4b8d14cc1349
kaschul <47723396+kaschul@users.noreply.github.com>
2022-02-28 12:41:27 -0800

    added files

--- show-me-app/src/app/app.module.ts
+++ show-me-app/src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ShowDisplayComponent } from './show-display/show-display.component';
+import { ShowSearchComponent } from './show-search/show-search.component';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShowDisplayComponent
+    ShowDisplayComponent,
+    ShowSearchComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- show-me-app/src/app/tv-maze.service.ts
+++ show-me-app/src/app/tv-maze.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { IShowDisplay } from './ishow-display';
+import { map } from 'rxjs/operators';
+
+import { environment } from 'src/environments/environment';
 import { ITvMazeData } from './itv-maze-data';
 
 @Injectable({
@@ -11,19 +13,20 @@ export class TvMazeService {
   constructor(private httpClient : HttpClient) { }
 
   getShowInfo(showNameEntered: string){
-    return  this.httpClient.get<ITvMazeData>
+    return this.httpClient.get<ITvMazeData>
     (`https://api.tvmaze.com/singlesearch/shows?q=${showNameEntered}`)
+    .pipe(map(data => this.transformToIShowDisplay(data) ))
   }
 
   private transformToIShowDisplay(data: ITvMazeData){
     return {
       showName: data.name,
       showLanguage: data.language,
-      showGenres: data.genres[0],
+      showGenres: data.genres,
       showStatus: data.status,
       showRuntime: data.runtime,
       showTime: data.schedule.time,
-      showDays: data.schedule.days[0],
+      showDays: data.schedule.days,
       showNetwork: data.network.name,
       showImage: data.image.medium,
       showSummary: data.summary
