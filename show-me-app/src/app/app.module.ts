b831346709d0ddeda6151fdd19b4a60f8f4a7a3a
kaschul <47723396+kaschul@users.noreply.github.com>
2022-02-27 23:02:31 -0800

    added files

--- show-me-app/src/app/app.module.ts
+++ show-me-app/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ShowDisplayComponent } from './show-display/show-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowDisplayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
