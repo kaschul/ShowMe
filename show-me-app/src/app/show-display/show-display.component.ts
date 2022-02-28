11dc5cdc681b3592d42722edc0233b1736b2bc63
kaschul <47723396+kaschul@users.noreply.github.com>
2022-02-28 14:04:41 -0800

    deleted unused impor

--- show-me-app/src/app/show-display/show-display.component.ts
+++ show-me-app/src/app/show-display/show-display.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { IShowDisplay } from '../ishow-display';
-import { TvMazeService } from '../tv-maze.service';
 
 @Component({
   selector: 'app-show-display',
