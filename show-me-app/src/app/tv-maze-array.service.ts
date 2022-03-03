26672a8d93920dc17315fc3479b391344da48a36
kaschul <47723396+kaschul@users.noreply.github.com>
2022-03-03 14:56:01 -0800

    cont

--- show-me-app/src/app/tv-maze-array.service.ts
+++ show-me-app/src/app/tv-maze-array.service.ts
@@ -27,4 +27,47 @@ export class TvMazeArrayService {
         return results as ShowArrayNames[]
     })
   }
+
+  populateSelect() {
+    var shows : [] = document.getElementById('sel');
+    for (var i = 0; i < shows.length; i++) {
+      ele.innerHTML = ele.innerHTML + '<option value="' + shows[i]['name'] + 
+    }
+    )
+  }
+}
+
+window.onload = populateSelect();
+
+function populateSelect() {
+
+    // CREATE AN XMLHttpRequest OBJECT, WITH GET METHOD.
+    var xhr = new XMLHttpRequest(), 
+        method = 'GET',
+        overrideMimeType = 'application/json',
+        url = `https://api.tvmaze.com/search/shows?q=${userInput}`;        // ADD THE URL OF THE FILE.
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            
+            // PARSE JSON DATA.
+            var showOptions = JSON.parse(xhr.responseText);
+
+            var ele = document.getElementById('sel');
+            for (var i = 0; i < showOptions.length; i++) {
+                // BIND DATA TO <select> ELEMENT.
+                ele.innerHTML = ele.innerHTML +
+                    '<option value="' + showOptions[i].ID + '">' + showOptions[i].Name + '</option>';
+            }
+        }
+    };
+    xhr.open(method, url, true);
+    xhr.send();
+}
+
+function display(ele) {
+    // GET THE SELECTED VALUE FROM <select> ELEMENT AND SHOW IT.
+    var msg = document.getElementById('msg');
+    msg.innerHTML = 'Selected Show: <b>' + ele.options[ele.selectedIndex].text + '</b> </br>' +
+        'ID: <b>' + ele.value + '</b>';
 }
