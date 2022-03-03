a09cc0694db407816d4b4ce11c14f0116011d0a3
kaschul <47723396+kaschul@users.noreply.github.com>
2022-03-02 17:27:30 -0800

    singlesearch

--- show-me-app/src/app/show-search/show-search.component.ts
+++ show-me-app/src/app/show-search/show-search.component.ts
@@ -10,14 +10,13 @@ import { debounceTime } from 'rxjs/operators';
 export class ShowSearchComponent implements OnInit {
   
   @Output() searchEvent = new EventEmitter<string>();
-  search = new FormControl('', [Validators.minLength(3)])
+  search = new FormControl('', [Validators.minLength(3)]);
  
   constructor() { }
 
   ngOnInit(): void {
-    this.search.valueChanges
-    .pipe(debounceTime(1000))
-    .subscribe((searchValue: string) => {
+    
+    this.search.valueChanges.pipe(debounceTime(1000)).subscribe((searchValue: string) => {
       if (!this.search.invalid){
         this.searchEvent.emit(searchValue)
       }
